03668cde38d8b28d97bd98afafeb6cea41df0fb2
gretkierewicz <piotr.gretkierewicz@gmail.com>
2020-05-10 14:32:48 +0200

    feat/logs - local log buffer

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,28 @@
+import os, logging
+
 from config import Config
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+
+#if not app.debug
+if app.debug:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/gret_notes.log', maxBytes=10480, backupCount=20)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('Gret_notes startup')
 
 from app import routes, models
