cc5d20c19eaf989343969d3d29c64999872210b4
gretkierewicz <piotr.gretkierewicz@gmail.com>
2020-05-10 18:18:09 +0200

    DB/tag - update

--- app/models.py
+++ app/models.py
@@ -8,7 +8,8 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
-	notes = db.relationship('Note', backref='author', lazy='dynamic')
+	notes = db.relationship('Note', backref='owner', lazy='dynamic')
+	tags = db.relationship('Tag', backref='owner', lazy='dynamic')
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
@@ -23,6 +24,11 @@ class User(UserMixin, db.Model):
 def load_user(id):
 	return User.query.get(int(id))
 
+note_tag = db.Table('note_tag',
+	db.Column('note_id', db.Integer, db.ForeignKey('note.id')),
+	db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
 class Note(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(128), index=True)
@@ -30,6 +36,15 @@ class Note(db.Model):
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	update_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	tags = db.relationship('Tag', secondary=note_tag, backref='tags')
 
 	def __repr__(self):
 		return '<Note {}>'.format(self.title)
+
+class Tag(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), index=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Tag {}>'.format(self.name)
